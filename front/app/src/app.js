d7f4c81034fc3b798ab4bd2b062628ddaaf035bc
Damien Cassan <damien.cassan@eisti.eu>
2016-04-18 20:28:59 +0200

    feat : Interface statique pour l'accueil

---
+++ front/app/src/app.js
@@ -0,0 +1,24 @@
+angular
+    .module('starterApp', ['ngMaterial', 'accueil', 'ngRoute'])
+    .config(function($mdThemingProvider, $mdIconProvider, $routeProvider){
+
+        $mdIconProvider
+            .defaultIconSet("./assets/svg/avatars.svg", 128)
+            .icon("menu"       , "./assets/svg/menu.svg"        , 24)
+            .icon("share"      , "./assets/svg/share.svg"       , 24)
+            .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+            .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+            .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+            .icon("phone"      , "./assets/svg/phone.svg"       , 512);
+
+        /*$mdThemingProvider.theme('default')
+         .primaryPalette('brown')
+         .accentPalette('red');*/
+
+        $routeProvider.
+        when('/phones', {
+            templateUrl: 'src/client/accueil/accueil.html',
+            controller: 'AccueilController'
+        })
+
+    });

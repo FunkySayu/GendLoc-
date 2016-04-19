26e27e426ec07c44d4b73dc8a61d789d58a270b8
Titouan BION <titouan.bion@gmail.com>
2016-04-19 20:26:16 +0200

    refact: backend refactor, useless lib removed

--- back/simple/src/simpleServer.js
+++ back/simple/src/simpleServer.js
@@ -1,7 +1,5 @@
-var express = require('express');
 var fs = require('fs');
-var http = require('http');
-var https = require('https');
+var express = require('express');
 var app = express();
 var options = {
     root: __dirname + '/',
@@ -12,7 +10,35 @@ var options = {
     }
 };
 
-var io = require('socket.io')(http);
+var io;
+
+//https
+try {
+    var privateKey = fs.readFileSync('privkey.pem', 'utf8');
+    var certificate = fs.readFileSync('cert.pem', 'utf8');
+    var credentials = {key: privateKey, cert: certificate};
+    var https = require('https');
+    var httpsServer = https.createServer(credentials, app);
+    io = require('socket.io').listen(httpsServer);
+    httpsServer.listen(443);
+} catch (e) {
+    console.log(e);
+    var http = require('http');
+    var server = http.createServer(app);
+    io = require('socket.io').listen(server);
+}
+
+app.get('/', function (req, res) {
+    res.redirect('app')
+});
+
+app.get('/js/:source', function (req, res) {
+    res.sendFile("js/" + req.params.source, options);
+});
+app.get('/views/:source', function (req, res) {
+    res.sendFile("views/" + req.params.source, options);
+});
+
 var nonAssignedSockets = {};
 var operatorsPool = {};
 var victimsSockets = {};
@@ -42,30 +68,7 @@ io.on('connection', function (socket) {
     });
 });
 
-app.get('/', function (req, res) {
-    res.redirect('app')
-});
-
-app.get('/js/:source', function (req, res) {
-    res.sendFile("js/" + req.params.source, options);
-});
-app.get('/views/:source', function (req, res) {
-    res.sendFile("views/" + req.params.source, options);
-});
-
 app.use(express.static('../front'));
 
 app.listen(8081, function () {
 });
-
-
-//https
-try {
-    var privateKey  = fs.readFileSync('privkey.pem', 'utf8');
-    var certificate = fs.readFileSync('cert.pem', 'utf8');
-    var credentials = {key: privateKey, cert: certificate};
-    var httpsServer = https.createServer(credentials, app);
-    httpsServer.listen(443);
-} catch (e){
-   console.log(e);
-}

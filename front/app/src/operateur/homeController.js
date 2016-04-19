33ada8a25ac4d2e6b6cb30be0a482f58a06958e7
Axel Martin <axel.martin@eisti.fr>
2016-04-19 09:34:39 +0000

    front: add: user selection

--- front/app/src/operateur/homeController.js
+++ front/app/src/operateur/homeController.js
@@ -4,10 +4,16 @@
         .module('operateur')
         .controller('HomeController', HomeController);
 
+    /**
+     * Global home controller
+     */
     function HomeController($scope, $mdDialog) {
 
+        $scope.users = [ ];
+        $scope.selected = null;
         $scope.images = [ ];
 
+        // XXX: for debug purpose only
         for (var i = 0; i < 10; ++i) {
             $scope.images.push({
                     url: "http://www.louisetzeliemartin.org/medias/images/chat-1.jpg",
@@ -15,27 +21,43 @@
                 });
         }
 
-        $scope.showImage = function (image) {
+        // XXX: for debug purpose only
+        for (i = 0; i < 10; ++i) {
+            $scope.users.push({
+                name: "Test Lorem",
+                phone: "+336 80 50 18 15"
+            })
+        }
 
-            // TODO: traiter l'image
+        /** Internal functions **/
+
+        /**
+         * Open a dialog and show the full sized image
+         */
+        $scope.showImage = function (image) {
             $mdDialog.show({
                 clickOutsideToClose: true,
                 templateUrl: 'src/operateur/popup.html',
                 locals: {
                     image: image
                 },
-                controller: function ($scope, image) {
-                    $scope.image = image;
-                }
+                controller: PopupDialogController
             });
         };
+
+        $scope.selectUser = function (user) {
+            $scope.selected = user;
+        }
     }
 
+    /**
+     * Image popup controller
+     */
     function PopupDialogController($scope, $mdDialog, image) {
         $scope.image = image;
 
         $scope.closeDialog = function () {
-        
+            $mdDialog.hide();
         }
     }
 })();

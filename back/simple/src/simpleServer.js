ef38756a0f2b8623a8b3ffe4c3bc0b09a2c108d0
Damien Cassan <damien.cassan@eisti.eu>
2016-04-19 20:35:30 +0200

    fix : Mise en place socket.io

--- back/simple/src/simpleServer.js
+++ back/simple/src/simpleServer.js
@@ -1,18 +1,20 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var options = {
-    root: __dirname + '/',
-    dotfiles: 'deny',
-    headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
-    }
-};
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var io;
+app.get('/', function (req, res) {
+    res.redirect('app')
+});
 
-//https
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+app.use(express.static('../front'));
+
 try {
     var privateKey = fs.readFileSync('privkey.pem', 'utf8');
     var certificate = fs.readFileSync('cert.pem', 'utf8');
@@ -23,52 +25,17 @@ try {
     httpsServer.listen(443);
 } catch (e) {
     console.log(e);
-    var http = require('http');
-    var server = http.createServer(app);
-    io = require('socket.io').listen(server);
-}
-
-app.get('/', function (req, res) {
-    res.redirect('app')
-});
-
-app.get('/js/:source', function (req, res) {
-    res.sendFile("js/" + req.params.source, options);
-});
-app.get('/views/:source', function (req, res) {
-    res.sendFile("views/" + req.params.source, options);
-});
-
-var nonAssignedSockets = {};
-var operatorsPool = {};
-var victimsSockets = {};
-
-function defineRole(informations) {
-    switch (informations['role']) {
-        case 'operateur':
-            console.log('User is now considered as Operator');
-            nonAssignedSockets.delete(socket.id);
-            operatorsPool[socket.id] = socket;
-            break;
-        case 'victime':
-            console.log('User is now considered as Victim');
-            nonAssignedSockets.delete(socket.id);
-            var victim = {};
-            victim[informations['numero']] = socket;
-            victimsSockets.push(victim);
-    }
-}
-
-io.on('connection', function (socket) {
-    console.log('User connected');
-    nonAssignedSockets[socket.id] = socket;
-    socket.on('role', defineRole);
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
+    http.listen(8081, function () {
     });
-});
+}
 
-app.use(express.static('../front'));
 
-app.listen(8081, function () {
-});
+
+var options = {
+    root: __dirname + '/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+};

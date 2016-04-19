e64cb46d2ef595218b16b77861a41a390dcb2576
Titouan BION <titouan.bion@gmail.com>
2016-04-20 00:20:48 +0200

    feature: get des fiches disponibles

--- front/app/src/operateur/services/fiches.js
+++ front/app/src/operateur/services/fiches.js
@@ -7,22 +7,6 @@
     function FicheService($q, $timeout) {
 
         // XXX: for debug purpose only
-        var fiches = [
-            {
-                name: "Une fiche reflexe 1",
-                keywords: ["toto", "lulz", "lmao"],
-                url: "pollution.jpg"
-            },
-            {
-                name: "Une fiche reflexe 2",
-                keywords: ["lulz"],
-                url: "pollution.jpg"
-            },
-            {
-                name: "Une fiche reflexe 3",
-                keywords: ["lulz", "lmao"],
-                url: "pollution.jpg"
-            }];
 
         var recupererFiches = function () {
             return $q.when(fiches);

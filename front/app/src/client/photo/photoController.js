b1ab51bdfa07ec400a7dc8a5986618304e795e67
Axel Martin <axel.martin@eisti.fr>
2016-04-20 00:49:03 +0000

    front: add: liaison controller - vue

--- front/app/src/client/photo/photoController.js
+++ front/app/src/client/photo/photoController.js
@@ -4,45 +4,8 @@ angular
 
 function PictureController($scope) {
 
-	var video,
-	videoObj,
-	errBack = function(error) {
-		console.log("Video capture error: ", error.code); 
-	};
-
-	function detect_safari() {
-        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1)
-            return true
-        return false
-	}
-
-	$scope.onloadvideo = function(elem) {
-
-		video = elem;
-		videoObj = { "video": true };
-				// Put video listeners into place
-		if(navigator.getUserMedia) { // Standard
-			navigator.getUserMedia(videoObj, function(stream) {
-				video.src = stream;
-				video.play();
-			}, errBack);
-		} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-			navigator.webkitGetUserMedia(videoObj, function(stream){
-				video.src = window.webkitURL.createObjectURL(stream);
-				video.play();
-			}, errBack);
-		}
-		else if(navigator.mozGetUserMedia) { // Firefox-prefixed
-			navigator.mozGetUserMedia(videoObj, function(stream){
-				video.src = window.URL.createObjectURL(stream);
-				video.play();
-			}, errBack);
-		}
-
-	}
-
-    $scope.videoUrl = undefined;
-    $scope.isSafari = false;
+    $scope.isSafari = navigator.userAgent.indexOf('Safari') != -1 &&
+        navigator.userAgent.indexOf('Chrome') == -1;
 
     function errorCallback(error) {
         console.log("Video capture error: ", error.code);
